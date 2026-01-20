a1ccb0744ed7d365b78b5c0ec0cc71a094140c78
Cheatahh <florian.stachnet@gmail.com>
2026-01-20 08:44:52 +0100

    1

---
+++ src/android/FidoWebview.kt
@@ -0,0 +1,52 @@
+package com.fkmit.fido
+
+import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaPlugin
+import org.json.JSONArray
+import org.json.JSONObject
+
+enum class StatusCodes(val code: Int, val resultCode: Int) {
+    Success(1, PluginResult.Status.OK),
+    Failure(2, PluginResult.Status.ERROR),
+    Progress(3, PluginResult.Status.OK)
+}
+
+class FidoWebview : CordovaPlugin() {
+    override fun execute(action: String, args: JSONArray, callback: CallbackContext) = when(action) {
+        "getAssertion" -> {
+            executeGetAssertion(args)
+            true
+        }
+        else -> false
+    }
+    private fun CallbackContext.sendStatusResult(code: StatusCodes, payload: Any) {
+        val result = PluginResult(code.resultCode, JSONObject().apply {
+            put("statusCode", code.code)
+            put("payload", payload)
+        })
+        result.setKeepCallback(code === StatusCodes.Progress)
+        sendPluginResult(result)
+    }
+    private inline fun CallbackContext.with(args: JSONArray, handler: (JSONArray) -> Unit): Boolean {
+        runCatching {
+            handler(args)
+        }.onSuccess { result ->
+            sendStatusResult(StatusCodes.Success, result)
+        }.onFailure { err ->
+            sendStatusResult(StatusCodes.Failure, err.message)
+        }
+        return true
+    }
+    private fun executeGetAssertion(args: JSONArray): String {
+        require(args.length() == 2) {
+            "Invalid parameters, expected <CLIENT_DATA> <USER_PIN>."
+        }
+        val clientData = requireNotNull(args.optString(0).takeIf(String::isNotBlank)) {
+            "Parameter <CLIENT_DATA> must be a string."
+        }
+        val userPin = requireNotNull(args.optString(1).takeIf(String::isNotBlank)) {
+            "Parameter <USER_PIN> must be a string."
+        }
+        return "Nice"
+    }
+}
---
+++ www/FidoWebview.js
@@ -0,0 +1,22 @@
+const exec = require('cordova/exec');
+
+const FidoWebview = {
+    StatusCodes: {
+        SUCCESS: 1, // payload: result
+        FAILURE: 2, // payload: exception
+        PROGRESS: 3  // payload: progress
+    },
+    getAssertion: function(clientData, userPin, onStatusChanged) {
+        try {
+            exec((result) => {
+                onStatusChanged(result.statusCode, result.payload);
+            }, (err) => {
+                onStatusChanged(this.StatusCodes.FAILURE, err);
+            }, 'FidoWebview', 'getAssertion', [clientData, userPin]);
+        } catch(err) {
+            onStatusChanged(this.StatusCodes.FAILURE, err);
+        }
+    }
+};
+
+module.exports = FidoWebview;
